0c1336d845b75e0dc371f57cf9db7060ed103846
Christian Caracach <christiancaracach@gmail.com>
2024-03-31 20:28:25 -0300

    added interactivity

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() => runApp(const MyApp());
 
@@ -73,15 +74,62 @@ class TitleSection extends StatelessWidget {
             ),
           ),
           /*3*/
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          const Text('41'),
+          const FavoriteWidget()
         ],
       ),
     );
   }
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+            padding: const EdgeInsets.all(0),
+            child: IconButton(
+              padding: const EdgeInsets.all(0),
+              alignment: Alignment.center,
+              icon: (_isFavorited
+                  ? const Icon(Icons.star)
+                  : const Icon(Icons.star_border)),
+              onPressed: _toggleFavorite,
+            )),
+        SizedBox(
+          width: 18,
+          child: SizedBox(
+            child: Text('$_favoriteCount'),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class FavoriteWidget extends StatefulWidget {
+  const FavoriteWidget({
+    super.key,
+  });
+
+  @override
+  State<FavoriteWidget> createState() => _FavoriteWidgetState();
 }
 
 class ButtonSection extends StatelessWidget {
